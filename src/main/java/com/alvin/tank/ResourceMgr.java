f40063771a499d5924290541b8a9f0eec99bc738
Alvin <conf1102@163.com>
2021-05-26 00:30:19 +0800

    Add ResourceMgr and change Tank to image

---
+++ src/main/java/com/alvin/tank/ResourceMgr.java
@@ -0,0 +1,24 @@
+package com.alvin.tank;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ResourceMgr {
+    public static BufferedImage tankL;
+    public static BufferedImage tankU;
+    public static BufferedImage tankR;
+    public static BufferedImage tankD;
+
+    static {
+        try {
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/main/java/com/alvin/tank/Tank.java
+++ src/main/java/com/alvin/tank/Tank.java
@@ -19,10 +19,21 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.YELLOW);
-        g.fillRect(x, y, 50, 50);
-        g.setColor(c);
+        switch (direction){
+            case LEFT:
+                g.drawImage(ResourceMgr.tankL, x, y, null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.tankU, x, y, null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.tankR, x, y, null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.tankD, x, y, null);
+                break;
+        }
+
         move();
 
     }
